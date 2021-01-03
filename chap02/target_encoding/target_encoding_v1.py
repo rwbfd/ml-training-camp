a0e364287b54f58883aede4377d8d971c1977789
rwbfd <ran.wang.math@gmail.com>
2021-01-03 18:22:54 +0800

    Fixed bugs in target encoding'

--- chap02/target_encoding/target_encoding_v1.py
+++ chap02/target_encoding/target_encoding_v1.py
@@ -11,11 +11,31 @@ def target_mean_v1(data, y_name, x_name):
     return result
 
 
+def target_mean_v2(data, y_name, x_name):
+    result = np.zeros(data.shape[0])
+    value_dict = dict()
+    count_dict = dict()
+    for i in range(data.shape[0]):
+        if data.loc[i, x_name] not in value_dict.keys():
+            value_dict[data.loc[i, x_name]] = data.loc[i, y_name]
+            count_dict[data.loc[i, x_name]] = 1
+        else:
+            value_dict[data.loc[i, x_name]] += data.loc[i, y_name]
+            count_dict[data.loc[i, x_name]] += 1
+    for i in range(data.shape[0]):
+        result[i] = (value_dict[data.loc[i, x_name]] - data.loc[i, y_name]) / (count_dict[data.loc[i, x_name]] - 1)
+    return result
+
+
 def main():
     y = np.random.randint(2, size=(5000, 1))
     x = np.random.randint(10, size=(5000, 1))
     data = pd.DataFrame(np.concatenate([y, x], axis=1), columns=['y', 'x'])
-    result = target_mean_v1(data, 'y', 'x')
+    result_1 = target_mean_v1(data, 'y', 'x')
+    result_2 = target_mean_v2(data, 'y', 'x')
+
+    diff = np.linalg.norm(result_1 - result_2)
+    print(diff)
 
 
 if __name__ == '__main__':
